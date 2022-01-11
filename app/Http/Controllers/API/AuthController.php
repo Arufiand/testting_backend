389d6c754bb32b91b927f29e3fdb297b554359c9
arufiand <alfiandannyarmanta@gmail.com>
2022-01-11 12:02:09 +0700

    Update Controller for Login and Register users

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Auth\Events\Validated;
+use Illuminate\Database\QueryException;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use PhpParser\Node\Stmt\TryCatch;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|min:8'
+        ]);
+        if ($validatedData) {
+        try {
+            $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+            ]);
+
+            return response()->json([
+            'User_data' => $user,
+            'Status' => True,
+            ], Response::HTTP_OK);
+            } catch (QueryException $e) {
+                return response()->json(
+                [
+                    'Status' => false,
+                    'Message' => "Failed " . $e->errorInfo
+                ],
+                Response::HTTP_NOT_ACCEPTABLE
+                );
+            }
+        } else {
+        return response()->json(
+            [
+            'Status' => false,
+            'Message' => "Failed "
+            ],
+            Response::HTTP_BAD_REQUEST
+        );
+        }
+    }
+
+    public function login(Request $req)
+	{
+		$validator = Validator::make(
+			$req->all(),
+			[
+				'email' => ['required'],
+				'password' => ['required']
+			]
+		);
+		if ($validator->fails()) {
+			return response()->json(
+				[
+					'Status_Request' => false,
+					'Message' => $validator->errors()
+				],
+				Response::HTTP_UNPROCESSABLE_ENTITY
+			);
+		}
+		try {
+			$user = User::where('email', $req->email)->first();
+			if (!$user || !Hash::check($req->password, $user->password)) {
+				return response()->json(['message' => "Wrong Email & Password Combination"], Response::HTTP_UNAUTHORIZED);
+			}
+			$token = $user->createToken('Auth-Token', ['server:update'])->plainTextToken;
+			$view = [
+				"Username" => $user->name,
+				"Email" => $user->email,
+				"Token" => $token
+			];
+			return response()->json([
+				'Message' => "Login Success",
+				'Status' => True,
+				'Data' => $view, 
+			], Response::HTTP_OK);
+            } catch (QueryException $e) {
+                    return response()->json(
+                        [
+                            'Status' => false,
+                            'Message' => "Failed ". $e->errorInfo 
+                        ]
+                    );
+            }
+	}
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+//Non Token Group API
+Route::post('/register', [authController::class, 'register']);
+Route::post('/login', [authController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('/logout', [authController::class, 'logout']);
 });
