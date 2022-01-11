59b6b3ce8754e1f6ee705d332702ee376616220e
arufiand <alfiandannyarmanta@gmail.com>
2022-01-11 12:26:50 +0700

    edit categories table

--- database/migrations/2022_01_11_050457_create_categories_table.php
+++ database/migrations/2022_01_11_050457_create_categories_table.php
@@ -15,6 +15,7 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string("c_name")->nullable();
             $table->timestamps();
         });
     }
